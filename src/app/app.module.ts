d6956f78e20b4794c0639ec0c234f190c632359b
Shradha Pandey <35809333+ShradhaPandey@users.noreply.github.com>
2018-10-04 02:27:45 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { HlHighlightDirective } from './other-highlight/hl-highlight.directive';
+import { BindOutsideHighlightDirective } from './bind-outside-highlight/bind-outside-highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+    HlHighlightDirective,
+    BindOutsideHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
